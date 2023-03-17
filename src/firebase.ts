6223db4beb9121270b488a4e3387b123eec788c6
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2023-03-17 21:10:51 +0900

    channelの中を表示 in feature/firebase2

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite"; // インポートする
+import { getFirestore } from "firebase/firestore"; // インポートする
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
