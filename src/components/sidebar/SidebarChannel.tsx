41ced859917d1af45fae11baadcdb8dc6c770a5b
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2023-03-18 22:33:59 +0900

    チャンネル名表示 in feature/firebase3

--- src/components/sidebar/SidebarChannel.tsx
+++ src/components/sidebar/SidebarChannel.tsx
@@ -9,11 +9,12 @@ type props = {
 
 function SidebarChannel(props: props) {
   const { id, channel } = props;
+  // console.log(channel)
   return (
     <div className="sidebarChannel">
       <h4>
         <span className="sidebarChannelHash">#</span>
-        {}
+        {channel.channel.channelName}
       </h4>
     </div>
   );
